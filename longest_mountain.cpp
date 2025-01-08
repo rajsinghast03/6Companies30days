cb53ba3459169daced8cbc10de5ecb9f74355b03
Raj Singh <rajsinghast03@gmail.com>
2025-01-08 17:15:08 +0530

    solved longest mountain

---
+++ longest_mountain.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+  int longestMountain(vector<int> &arr)
+  {
+    int n = arr.size();
+    vector<int> dp1(n, 1), dp2(n, 1);
+
+    int ans = 0;
+    for (int i = 1; i < n; ++i)
+    {
+      if (arr[i] > arr[i - 1])
+        dp1[i] = 1 + dp1[i - 1];
+    }
+    for (int i = n - 2; i >= 0; --i)
+    {
+      if (arr[i] > arr[i + 1])
+        dp2[i] = 1 + dp2[i + 1];
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+      if (dp1[i] > 1 && dp2[i] > 1)
+        ans = max(ans, dp1[i] + dp2[i] - 1);
+    }
+
+    return ans;
+  }
+};
